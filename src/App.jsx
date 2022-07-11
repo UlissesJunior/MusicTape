f8aa181baf6933b0882f17f3dbe9e09d0d949c3c
UlissesJunior <ulissesjunior2005@gmail.com>
2022-07-11 20:02:33 -0300

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import './styles/app.css'
+import "./styles/app.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-    <div className="center_high">
-      <Link to ="/play">
-      Play
-      </Link>
+    <div className="container-max">
+      <div>
+        <h1>
+          Music <br /> <span>Tape</span>
+        </h1>
+      </div>
+      <div className="right">
+        <Link to="/play">
+          <h2>To Play</h2>
+        </Link>
+        <h2>To Study</h2>
+        <h2>To Relax</h2>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
