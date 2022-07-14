db9e142c22fbe685f0fc73dbbe890e745bdff56c
UlissesJunior <ulissesjunior2005@gmail.com>
2022-07-14 19:30:31 -0300

    error :(

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -20,9 +20,6 @@ function App() {
         <Link to="/torelax">
         <h2>To Relax</h2>
         </Link>
-        <Link to="/api/toplay">
-        <h2>API</h2>
-        </Link>
       </div>
     </div>
   );
