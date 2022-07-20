b34e48cbe40a9cfbd8a202fad1780042712d22de
UlissesJunior <ulissesjunior2005@gmail.com>
2022-07-20 16:03:12 -0300

    fix range in volume

--- src/components/volume.jsx
+++ src/components/volume.jsx
@@ -2,26 +2,26 @@ import "../styles/app.css";
 import { Howler } from "howler";
 import { useState } from "react";
 
-
 function Volume() {
-  const [vol, setVol] = useState(1)
-  const volFixed = vol*100;
+  const [vol, setVol] = useState(1);
+  const volFixed = vol * 100;
+  const volView = volFixed.toFixed(0);
   Howler.volume(vol);
 
-   document.body.onkeyup = function (e) {
-    if (e.keyCode == 38) {
+  document.body.onkeyup = function (e) {
+    if (e.keyCode == 38 && volView < 100) {
       console.log("Aumentando o Volume");
-      setVol(vol + 0.1)
+      setVol(vol + 0.1);
     }
-    if (e.keyCode == 40) {
+    if (e.keyCode == 40 && volView > 0) {
       console.log("Abaixando o Volume");
-      setVol(vol - 0.1)
+      setVol(vol - 0.1);
     }
   };
 
   return (
     <>
-      <div onClick={() => setVol(vol + 0.1)} className="music-playing">{volFixed.toFixed(0)}</div>
+      <div className="music-playing">{volView}</div>
     </>
   );
 }
